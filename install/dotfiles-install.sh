ad4fc2a7c4cee3a5830909b595bb100472cd7f33
Dennis Geiselmann <dennis.geiselmann@outlook.de>
2020-08-13 17:35:16 +0200

    move all dotfiles to the same folder

--- install/dotfiles-install.sh
+++ install/dotfiles-install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-echo_message "Create symlinks of dotfiles to home directory"
-ln -sfv "$(pwd)/etc/bash/.bash_aliases" ~
-ln -sfv "$(pwd)/etc/bash/.bashrc" ~
-ln -sfv "$(pwd)/etc/readline/.inputrc" ~
-ln -sfv "$(pwd)/etc/git/.gitconfig" ~
-ln -sfv "$(pwd)/etc/git/.gitignore" ~
-ln -sfv "$(pwd)/etc/git/.gitattributes" ~
-ln -sfv "$(pwd)/etc/vim/.vimrc" ~
+echo_message "Create symlinks of dotfiles to home directory";
+# Get all dotfiles except the 'dot' and 'dot dot' dir 
+DOTFILES=/$(pwd)/etc/dotfiles/.[^.]*
 
-echo_message "Copy .vim folder to home directory"
-cp -r "$(pwd)/etc/vim/.vim/" ~
+for file in $DOTFILES; do
+    ln -sfv $file ~
+done
 
-source ~/.bashrc
+echo_message "Copy .vim folder to home directory";
+# cp -r "$(pwd)/etc/.vim/" ~;
 
-echo_message "Don't forget to create your .gitconfig.local"
+# source ~/.bash_profile;
+
+echo_message "Don't forget to create your .gitconfig.local";
