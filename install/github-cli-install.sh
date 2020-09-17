d411d0797382ac297d682ec850b06405316307b8
Dennis Geiselmann <dennis.geiselmann@outlook.de>
2020-09-17 22:44:05 +0200

    update github-cli version to 1.0.0

--- install/github-cli-install.sh
+++ install/github-cli-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -P ~/ --no-hsts https://github.com/cli/cli/releases/download/v0.11.1/gh_0.11.1_linux_amd64.deb
+wget -P ~/ --no-hsts https://github.com/cli/cli/releases/download/v1.0.0/gh_1.0.0_linux_amd64.deb
 
-sudo apt install ./gh_0.11.1_linux_amd64.deb
-check_successful $? "GitHub CLI v0.11.1"
+sudo apt install ~/gh_1.0.0_linux_amd64.deb
+check_successful $? "GitHub CLI v1.0.0"
