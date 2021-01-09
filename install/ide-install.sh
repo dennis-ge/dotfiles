954e608ea97a847ea177523250b28aab5480a9dd
Dennis Geiselmann <dennis.geiselmann@sap.com>
2021-01-09 11:36:33 +0100

    add mac for IntelliJ IDE's

--- install/ide-install.sh
+++ install/ide-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Abort if the System is not Ubuntu Desktop
-is_ubuntu_desktop || exit 1
+is_ubuntu_desktop | is_macos || exit 1
 
 function read_input {
 	read -n1 -r -p " $1 (Y/n) " key
@@ -14,26 +14,38 @@ function read_input {
 }
 
 # JetBrains Products
-if read_input "Install PyCharm?"; then
-	echo_message "Starting to install snap package 'PyCharm Professional'"
-	sudo snap install pycharm-professional --classic
+if read_input "Install PyCharm CE?"; then
+	echo_message "Starting to install PyCharm CE"
+	if is_macos; then
+		brew install --cask pycharm-ce
+	else
+	sudo snap install pycharm-community --classic
+	pycharm-professional_pycharm-professional.desktop >> ../etc/favorite_apps/favorite_apps.txt
+	fi
 	check_successful $? "PyCharm Professional"
 	new_small_separator
-	pycharm-professional_pycharm-professional.desktop >> ../etc/favorite_apps/favorite_apps.txt
 fi
 
 if read_input "Install WebStorm?"; then
-	echo_message "Starting to install snap package 'WebStorm'"
-	sudo snap install webstorm --classic
+	echo_message "Starting to install WebStorm"
+	if is_macos; then
+		brew install --cask webstorm
+	else
+		sudo snap install webstorm --classic
+		webstorm_webstorm.desktop >> ../etc/favorite_apps/favorite_apps.txt
+	fi
 	check_successful $? "Webstorm"
 	new_small_separator
-	webstorm_webstorm.desktop >> ../etc/favorite_apps/favorite_apps.txt
 fi
 
 if read_input "Install Goland?"; then
-	echo_message "Starting to install snap package 'Goland'"
-	sudo snap install goland --classic
+	echo_message "Starting to install Goland"
+	if is_macos; then
+		brew install --cask goland
+	else
+		sudo snap install goland --classic
+		goland_goland.desktop >> ../etc/favorite_apps/favorite_apps.txt
+	fi
 	check_successful $? "Goland"
 	new_small_separator
-	goland_goland.desktop >> ../etc/favorite_apps/favorite_apps.txt
 fi
